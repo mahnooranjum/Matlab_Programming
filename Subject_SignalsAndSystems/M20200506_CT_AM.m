e5d1040a51df3e6241f3512a19c28bcc7b71a32c
Mahnoor Anjum <manomaq@gmail.com>
2020-05-10 07:06:52 +0500

    updated signals and systems

--- Subject_SignalsAndSystems/M20200506_CT_AM.m
+++ Subject_SignalsAndSystems/M20200506_CT_AM.m
@@ -1,64 +1,92 @@
 clear
-
-
-
-fs = 512; % Sampling frequency
+fs = 6000; % Sampling frequency
 sps = 1/fs; % Seconds per sample  
-stoptime = 0.1; % seconds 
-t = (-0.1:sps:stoptime)'; % seconds 
+stoptime = 10000; % seconds 
+n = (-stoptime:1:stoptime)'; % seconds 
+L = length(n);
+ft_axis_hz = fs * ((-L/2:(L/2) - 1)/L);
 
-message = cos((2*pi*t)) + (0.5 * cos((4*pi*t)))...
-    + (0.5 * cos((8*pi*t)));
+%% 
+message = cos((2*pi*n/fs)) + (0.5 * cos((4*pi*n/fs)))...
+                + (0.5 * cos((8*pi*n/fs)));
 
+fft_message = fftshift(fft(message)/L);
 
-F = 1000; % Frequency (hertz) 
-carrier = cos((2*pi*F*t)); %carrier
-
-
-subplot(2,2,1);
+figure 
+subplot(2,1,1);
 hold on
-plot(t,carrier);
-plot(t, message);
-legend("carrier", "message");
+plot(n/fs, message, 'linew', 2);
+legend("message");
 title('Time Domain Plots'); 
 xlabel('Time t'); 
 ylabel('f(t)');
 grid on
 
-l = length(t);
-fft_carrier = fftshift(fft(carrier)/l);
-fft_message = fftshift(fft(message)/l);
-ft_axis_hz = fs * ((-l/2:(l/2) - 1)/l);
-
-subplot(2,2,2);
-plot(ft_axis_hz, fft_carrier,ft_axis_hz, fft_message);
-legend("carrier", "message");
+subplot(2,1,2);
+plot(ft_axis_hz,abs(fft_message));
+legend("message");
 title('Frequency Domain Plots'); 
 xlabel('Frequency(Hertz)'); 
 ylabel('FFT(f(t))');
+axis([-50 50 -0.1 0.6])
  grid on
 
+
+%% 
+F = 1000; % Frequency (hertz) 
+carrier = cos((2*pi*F*n/fs)); %carrier
+fft_carrier = fftshift(fft(carrier)/L);
+
+figure
+subplot(2,1,1);
+hold on
+plot(F*n/fs,carrier);
+legend("carrier");
+title('Time Domain Plots'); 
+xlabel('Time t'); 
+ylabel('f(t)');
+axis([-30 30 -1.1 1.1])
+grid on
+
+subplot(2,1,2);
+plot(ft_axis_hz, abs(fft_carrier));
+legend("carrier");
+title('Frequency Domain Plots'); 
+xlabel('Frequency(Hertz)'); 
+ylabel('FFT(f(t))');
+axis([-1100 1100 -0.1 0.6])
+ grid on
+
+ %%
 %modulated = modulate(message,F,fs);
 mod_index= 1; % for 100% modulation
 Am=max(message); % Amplitude of modulating signal
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-modulated = Am .* (message.*carrier);
+modulated = (message.*carrier);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-subplot(2,2,3);
-plot(t,modulated);
-legend("modulated");
+
+figure
+subplot(2,1,1);
+plot(n,modulated);
+hold on
+plot(n, message, 'r');
+plot(n, -1.* message, 'r');
+
+legend("modulated", "envelope");
 title('AM Modulation Time Domain'); 
 xlabel('Time t'); 
 ylabel('m(t)');
 grid on
 
 
-fft_modulated= fftshift(fft(modulated)/l);
-subplot(2,2,4);
+fft_modulated= abs(fftshift(fft(modulated)/L));
+subplot(2,1,2);
 plot(ft_axis_hz, fft_modulated);
 legend("fft of modulated signal");
 title('FFT Modulation'); 
 xlabel('Frequency(Hertz)'); 
 ylabel('FFT(m(t))');
+axis([-1100 1100 -0.1 0.6])
  grid on
 
