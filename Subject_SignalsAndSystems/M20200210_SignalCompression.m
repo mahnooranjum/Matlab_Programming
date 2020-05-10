2f68661b6b7e12fd0db22fac13b1b91e5f95427d
Mahnoor Anjum <manomaq@gmail.com>
2020-05-11 00:35:20 +0500

    updated signals and systems

---
+++ Subject_SignalsAndSystems/M20200210_SignalCompression.m
@@ -0,0 +1,65 @@
+clc
+clear
+close all
+[raw,Fs]=audioread('man1.wav');
+
+L = length(raw);
+disp("Q1 - Read the audio file into MATLAB. What is the sampling frequency F_s of the audio signal?");
+disp("A1 - The sampling frequency is " + Fs + "Hz");
+
+disp("Q2 - What is the length of the audio signal? Plot the signal");
+disp("A2 - The length of the signal is " + L);
+
+figure
+plot(raw, 'b');
+hold on, grid on
+xlabel('time axis');
+ylabel('waveform value');
+
+%% Frequency Spectrum 
+sps = 1/Fs;
+stoptime = L; 
+n = (0:1:stoptime)';
+ft_axis_hz = Fs * ((-L/2:(L/2) - 1)/L);
+RAW = fftshift(fft(raw/L));
+
+figure
+plot(ft_axis_hz,abs((RAW)), 'b');
+legend("raw input wave file");
+title('Frequency Domain Plot'); 
+xlabel('Frequency(Hertz)'); 
+ylabel('FFT(f(t))');
+ grid on
+ 
+
+%% Select first k frequency components and set the rest to 0
+K = 15000;
+ft_axis_hz_K = ft_axis_hz((floor(L/2))-K:(floor(L/2))+K);
+RAW_K = RAW((floor(L/2))-K:(floor(L/2))+K);
+
+L_K = length(RAW_K);
+figure
+plot(ft_axis_hz_K,abs((RAW_K)), 'b');
+legend("raw input wave file");
+title('Frequency Domain Plot'); 
+xlabel('Frequency(Hertz)'); 
+ylabel('FFT(f(t))');
+grid on;
+
+%% Reconstruct signal
+
+raw1 = ifft(fftshift(RAW_K),L_K,'symmetric').*L_K;
+
+figure
+plot(raw1, 'b');
+hold on, grid on
+xlabel('time axis');
+ylabel('waveform value');
+
+hplayer = audioplayer(raw1,ceil(Fs*((K*2)/L)));
+play(hplayer);
+
+%% Get compression ratio
+
+ratio = 100 - (((K*2)/(L)) * (100) );
+
