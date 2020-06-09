52d777adffdcf57df710b07f48f148f53553a7cf
Mahnoor Anjum <manomaq@gmail.com>
2020-06-09 20:29:38 +0500

    Added file

---
+++ Subject_PreCalculus/d1_limits.m
@@ -0,0 +1,12 @@
+clear
+syms x
+y = piecewise(x~=1, (x-1)./(x-1));
+
+substitute = -2:0.1:2;
+y = subs(y, x,substitute);
+
+grid on, hold on, xlim([-2,2]), ylim([0,3])
+scatter(substitute,y, 'LineWidth', 2);
+dim = [.2 .5 .3 .3];
+str = ['Lim y when x --> 1 is 1'];
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
