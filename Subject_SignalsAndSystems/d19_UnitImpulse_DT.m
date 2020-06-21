1163e40ab91a55ba44a7afb7b3862769d47716ba
Mahnoor Anjum <manomaq@gmail.com>
2020-06-21 15:37:39 +0500

    added file

---
+++ Subject_SignalsAndSystems/d19_UnitImpulse_DT.m
@@ -0,0 +1,12 @@
+syms t n
+
+figure()
+val = -10:10;
+
+x = piecewise(n==0, 1, n~=0, 0);
+y = subs(x, n, val);
+
+hold on, grid on, box on, ylim([0 2]),xlim([-10 10]), 
+stem(val,y,'c', 'LineWidth', 2)
+
+legend("Unit Impulse")
