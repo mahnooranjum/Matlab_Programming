5adc6fb50b5b43ed978c7b912c9803188b4b44db
Mahnoor Anjum <manomaq@gmail.com>
2020-06-22 22:03:45 +0500

    added file

---
+++ Subject_SignalsAndSystems/d34_FundamentalFrequency_DT.m
@@ -0,0 +1,25 @@
+syms t n x
+val =  -20:0.01:20;
+
+sigma = pi*5/8;
+x = exp((1i * sigma * t));
+output = subs(x, t , val);
+
+valn =  -20:20;
+Ts = 2/3;
+xn = exp((1i * sigma * Ts * n));
+outputn = subs(xn, n , valn);
+% x2 = t;
+% y = cosh(x);
+% input2 = subs(x2, t , val);
+% output2 = subs(y, x , input2);
+
+
+kbym = sigma * Ts / (2*pi);
+fprintf('k / m:  %s\n',strtrim(rats(kbym)))
+
+hold on, grid on
+plot(val,output,'b', 'LineWidth', 2)
+stem(valn*Ts,outputn,'r', 'LineWidth', 2)
+legend("exp((i*5*pi*t/8) repeats at 24th step for Ts = 2/3")
+
