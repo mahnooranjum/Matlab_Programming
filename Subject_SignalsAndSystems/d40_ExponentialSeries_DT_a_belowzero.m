37dc8ed9d5c2b533d0d34a87cb3176a236b6b9c7
Mahnoor Anjum <manomaq@gmail.com>
2020-06-22 22:04:28 +0500

    added file

---
+++ Subject_SignalsAndSystems/d40_ExponentialSeries_DT_a_belowzero.m
@@ -0,0 +1,14 @@
+syms t n
+
+hold on; grid on;
+val = -40:40;
+a = 0.9;
+x = a^(n);
+y1 = subs(x, n, val);
+
+x = a^(-1*n);
+y2 = subs(x, n, val);
+stem(val,y1,'b', 'LineWidth', 2)
+stem(val,y2,'r', 'LineWidth', 2)
+legend("n +ve", "n -ve");
+
