9dd78b130afb15db2ead1dce4a58e7861895166b
Mahnoor Anjum <manomaq@gmail.com>
2020-06-23 19:46:02 +0500

    added file

---
+++ Subject_SignalsAndSystems/d46_CT_scale2.m
@@ -0,0 +1,15 @@
+syms x(t) 
+hold on, grid on, box on
+
+x(t) = sin(t);
+val =  linspace(-1,5,1000);
+y = subs(x(t),t,val);
+
+plot(val,y,'b', 'LineWidth', 2)
+
+x(t) = sin(1.1*t);
+y = subs(x(t),t,val);
+
+plot(val,y,'r', 'LineWidth', 2)
+
+legend("Original", "1.1 * t")
