0141c8338242e13203ed0bb3f1546751b1978b72
Mahnoor Anjum <manomaq@gmail.com>
2020-06-27 11:54:36 +0500

    added file

---
+++ Subject_SignalsAndSystems/d92_CT_Area_sin.m
@@ -0,0 +1,16 @@
+syms x(t) X(t)
+hold on, grid on, box on
+
+T0 = 1/4;
+w0 = 2*pi/T0;
+x(t) = cos(t);
+X(t) = int(x(t));
+
+val =  linspace(-5,5,1000);
+y = subs(x(t),t,val);
+Y = subs(X(t),t,val);
+
+hold on, grid on
+plot(val,y,'b:', 'LineWidth', 5)
+plot(val,Y,'r-', 'LineWidth', 3)
+legend("Real Signal", "Integral")
