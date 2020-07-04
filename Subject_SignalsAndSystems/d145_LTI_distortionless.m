65ede7d5e37ee8c1e02650b36e0de1d79c37e4e5
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 15:03:46 +0500

    added file

---
+++ Subject_SignalsAndSystems/d145_LTI_distortionless.m
@@ -0,0 +1,31 @@
+clear
+syms H(w)
+
+
+k = 10;
+t0 = 1;
+H(w) = exp(w*3);
+
+% for w1 = 1
+
+w1 = 1.5;
+phase = angle(H(-1i*w));
+group_delay = diff(-phase);
+
+divs = 100;
+val = 1/divs:1/divs:2;
+
+op_pd = subs(phase,w,val);
+op_gd = subs(group_delay,w,val);
+
+
+
+hold on, grid on
+
+subplot(1,2,1), hold on, grid on, box on
+plot(val, op_pd ,'b.-', 'LineWidth', 3)
+legend("phase H(jw) = - w*t0")
+
+subplot(1,2,2),hold on, grid on, box on
+plot(val, op_gd ,'b.-', 'LineWidth', 3)
+legend("group delay H(jw)")
