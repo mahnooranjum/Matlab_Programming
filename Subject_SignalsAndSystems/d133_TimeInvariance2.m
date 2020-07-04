9c0a76720b2ae1f08e952b1a50c880d42df944dc
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 15:02:13 +0500

    added file

---
+++ Subject_SignalsAndSystems/d133_TimeInvariance2.m
@@ -0,0 +1,22 @@
+clear
+syms i(t) T x(t) y(t) t x1(t) x2(t)
+
+hold on, grid on, box on
+
+x1(t) = t^2;
+
+divs = 100;
+val = -5:1/divs:5;
+
+t0 = 2;
+
+x(t) = x1(t);     T = x(t-1) +  x(t+1); y(t) = T; op = subs(y(t),t,val);
+x(t) = x1(t-t0);   T = x(t-1) +  x(t+1); y(t) = T; op_del = subs(y(t),t,val);
+hold on, grid on
+
+dely = op;
+shifter = length(1/divs:1/divs:t0);
+
+plot(val(1:(end-shifter+1)), dely(shifter:end) ,'b:', 'LineWidth', 4)
+plot(val, op_del ,'r-', 'LineWidth', 2)
+legend("y(t - t_0)", "T[ x(t - t_0) ]")
