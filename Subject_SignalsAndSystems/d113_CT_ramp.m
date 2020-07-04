0beff26c6f11a948b3244c9b9428fd9ccf7712c3
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 14:59:49 +0500

    added file

---
+++ Subject_SignalsAndSystems/d113_CT_ramp.m
@@ -0,0 +1,16 @@
+clear
+syms x(t) x_d(t)
+hold on, grid on, box on
+
+T0 = 1;
+w0 = 2*pi/T0;
+
+x(t) = (t)*heaviside(t);
+x_d(t) = diff(x(t));
+val =  linspace(-2,2,1000);
+y = subs(x(t),t,val);
+y_d = subs(x_d(t),t,val);
+hold on, grid on
+plot(val,y,'b:', 'LineWidth', 5)
+plot(val, y_d,'r-', 'LineWidth', 2)
+legend("Signal", "Derivative")
