b86cf26fd6bf97fbf70dfc9e3f72ef23c595d585
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 14:59:42 +0500

    added file

---
+++ Subject_SignalsAndSystems/d112_CT_signum.m
@@ -0,0 +1,16 @@
+clear
+syms x(t) x_d(t)
+hold on, grid on, box on
+
+T0 = 1;
+w0 = 2*pi/T0;
+
+x(t) = sign(t);
+x_d(t) = (x(t)+1);
+val =  linspace(-2,2,1000);
+y = subs(x(t),t,val);
+y_d = subs(x_d(t),t,val);
+hold on, grid on
+plot(val,y,'b:', 'LineWidth', 5)
+plot(val, y_d,'r-', 'LineWidth', 2)
+legend("Signal", "Signal + 1")
