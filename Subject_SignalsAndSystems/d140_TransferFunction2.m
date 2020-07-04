6d80dea0dee379b13b1994b1141b776664b76697
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 15:03:12 +0500

    added file

---
+++ Subject_SignalsAndSystems/d140_TransferFunction2.m
@@ -0,0 +1,22 @@
+clear
+syms H(w)
+
+
+k = 10;
+t0 = 1;
+H(w) = k*exp(-1i*w*t0);
+
+divs = 100;
+val = -10:1/divs:10;
+
+op_h = subs(H(w),w,val);
+
+hold on, grid on
+
+subplot(1,2,1), hold on, grid on, box on
+plot(val, abs(op_h) ,'b.-', 'LineWidth', 3)
+legend("magnitude H(jw)")
+
+subplot(1,2,2),hold on, grid on, box on
+plot(val, angle(op_h) ,'b.-', 'LineWidth', 3)
+legend("angle H(jw)")
