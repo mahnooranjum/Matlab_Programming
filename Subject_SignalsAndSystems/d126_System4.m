43de061665deb98b0c033b87411bc7010eb8bd97
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 15:01:32 +0500

    added file

---
+++ Subject_SignalsAndSystems/d126_System4.m
@@ -0,0 +1,19 @@
+clear
+syms i(t) T x(t) y(t)
+
+hold on, grid on, box on
+
+x(t) = heaviside(t);
+
+T = int(x(t));
+
+y(t) = T;
+
+val =  linspace(-5,5,1000);
+ip = subs(x(t),t,val);
+op = subs(y(t),t,val);
+
+hold on, grid on
+plot(val, ip ,'b:', 'LineWidth', 4)
+plot(val, op ,'r-', 'LineWidth', 2)
+legend("x(t)", "y(t)")
