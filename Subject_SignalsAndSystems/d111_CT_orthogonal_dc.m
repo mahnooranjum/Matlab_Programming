6ac0455a86f51a40ed5b82d2e7f2b028abc7936d
Mahnoor Anjum <manomaq@gmail.com>
2020-07-04 14:59:36 +0500

    added file

---
+++ Subject_SignalsAndSystems/d111_CT_orthogonal_dc.m
@@ -0,0 +1,17 @@
+clear
+syms x(t) x1(t) x2(t) X(t)
+hold on, grid on, box on
+
+T0 = 1;
+w0 = 2*pi/T0;
+x1(t) = sin(w0*t + 4);
+x2(t) = 3;
+x(t) = x1(t)*x2(t);
+O(t) = int(x(t),[0, T0]);
+val =  linspace(-2,2,1000);
+y = subs(x(t),t,val);
+
+hold on, grid on
+plot(val,y,'b:', 'LineWidth', 5)
+stem(0, O(t),'r-', 'LineWidth', 2)
+legend("Signal", "Orthogonal")
