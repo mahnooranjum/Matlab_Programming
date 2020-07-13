f15077f9f7ff97c9c8c24f6e26ee504edbfa2cac
Mahnoor Anjum <manomaq@gmail.com>
2020-07-13 22:41:53 +0500

    added file

---
+++ Subject_Plotting/d11_errorbar_bar.m
@@ -0,0 +1,18 @@
+clear 
+D=5;
+data = randn(10,D);
+
+subplot(2,2,1), axis square, hold on, grid on, box on
+bar(1:D, mean(data), 'b')
+
+subplot(2,2,2), axis square, hold on, grid on, box on
+bar(1:D, std(data), 'r')
+
+subplot(2,2,3), axis square, hold on, grid on, box on, xlim([0 D+1])
+errorbar(1:D, mean(data), std(data), 'k')
+
+subplot(2,2,4), axis square, hold on, grid on, box on, xlim([0 D+1])
+bar(1:D, mean(data), 'b')
+errorbar(1:D, mean(data), std(data), 'k')
+
+
