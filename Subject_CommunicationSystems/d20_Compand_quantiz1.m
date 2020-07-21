f8f64973a0d8f15db20885f735034c1b0b54d244
Mahnoor Anjum <manomaq@gmail.com>
2020-07-22 00:12:15 +0500

    added file

---
+++ Subject_CommunicationSystems/d20_Compand_quantiz1.m
@@ -0,0 +1,42 @@
+
+clear
+
+t = -2:1/100:2;
+x = exp(t*pi);
+levels = 2^2;
+codebook = [];
+delta = (max(x) - min(x))/levels;
+codebook = [min(x)];
+for i=1:levels-1
+    codebook = [codebook min(x)+i*delta];
+end
+
+[partition, codebook2] = lloyds(x,codebook);
+subplot(1,2,1), hold on, grid on, box on
+plot(t,x, 'r', 'linew',2)
+for i=1:length(codebook)
+    yline(codebook(i));
+end
+
+[x_q, quants] = quantiz(x, partition, codebook);
+plot(t,quants, 'b', 'linew',2)
+
+
+x = compand(x,255,max(x));
+levels = 2^2;
+codebook = [];
+delta = (max(x) - min(x))/levels;
+codebook = [min(x)];
+for i=1:levels-1
+    codebook = [codebook min(x)+i*delta];
+end
+
+[partition, codebook2] = lloyds(x,codebook);
+subplot(1,2,2), hold on, grid on, box on
+plot(t,x, 'r', 'linew',2)
+for i=1:length(codebook)
+    yline(codebook(i));
+end
+
+[x_q, quants] = quantiz(x, partition, codebook);
+plot(t,quants, 'b', 'linew',2)
