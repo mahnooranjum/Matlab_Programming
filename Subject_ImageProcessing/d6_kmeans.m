2502649a4935e3b0aa9a54075aaaae8bd8d752e2
Mahnoor Anjum <manomaq@gmail.com>
2023-02-05 21:17:18 +0500

    added

--- Subject_ImageProcessing/d6_kmeans.m
+++ Subject_ImageProcessing/d6_kmeans.m
@@ -1,20 +1,20 @@
 clear
 
 f = figure()
-img = imread('images/segmentation/cells.png');
+img = imread('images/segmentation/bts.jpg');
 subplot(2,2,1), imshow(img);
 
 gs = img(:,:,2);
 figure(f), subplot(2,2,2), imagesc(gs), colormap gray, axis square, xticks([]), yticks([])
 
-[L,Centers] = imsegkmeans(gs,3);
+[L,Centers] = imsegkmeans(gs,2);
 map = labeloverlay(gs,L);
 figure(f), subplot(2,2,3)
 imshow(map)
 title('Labeled Image')
 
 
-subplot(2,2,4), hold on
+subplot(2,2,4), hold on, axis ij
 imagesc(gs), colormap gray, axis square, xticks([]), yticks([])
 contour(map(:,:,1), 1, 'r')
 
